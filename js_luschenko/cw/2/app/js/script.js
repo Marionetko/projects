2e92e9b43aca25b2c2e1f9d501cc1d8388d684eb
Chebotaiev Anton <webius.net@gmail.com>
2022-04-05 16:33:46 +0300

    js_luschenko/ cw updated

--- js_luschenko/cw/2/app/js/script.js
+++ js_luschenko/cw/2/app/js/script.js
@@ -7,12 +7,12 @@ let inputIn = document.querySelector('.input-in'); // input
 let button = document.querySelector('button'); // button
 let div = document.querySelector('.out');
 
-button.onclick = function () {
+button.onclick = function (){
     // кнопка будет нажата
-    console.log('Работает!!!!!!');
-    // console.log(inputIn.value); // value - то, что введено в input
-    let b = +inputIn.value; // + перевести в число!!!!!!!
-    console.log(b + 10); // '66'+10 = 6610
+    console.log('Работает');
+    // console.log(inputIn.value); // value - то что введено в input
+    let b = +inputIn.value; // + перевести в число
+    console.log(b + 10); // '55' + 10 = 5510
     div.innerHTML = b;
     inputIn.value = '';
 }
