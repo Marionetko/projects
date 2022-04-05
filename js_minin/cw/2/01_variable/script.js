901f06e378ac4fdfc1fab7cd43df37b78b863976
Chebotaiev Anton <webius.net@gmail.com>
2022-04-05 13:27:17 +0300

    variable

---
+++ js_minin/cw/2/01_variable/script.js
@@ -0,0 +1,13 @@
+var message = 'Hello world! 2';
+var Message = ''; // регистр в названии имеет значение
+var helloMessage = 'Hello again'; // CamelCase - записываем переменные только так
+
+var $ = 'Dollar'; // Такие названия переменных тоже возможны
+var _ = 'Underscore';
+
+var message2 = 'Hello world message 2';
+var message3 = "Hello world message 3"; // Лучше писать черезе одинарные кавычки, лучше читабельность
+
+var сообщение = 'Сообщение'; // Плохой тон, но тоже работает
+
+console.log(сообщение); 
