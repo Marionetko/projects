656af0a38982a2975fa7ec698af59930fa9de7c3
Chebotaiev Anton <webius.net@gmail.com>
2022-07-04 15:54:00 +0300

    t7 done

--- js_luschenko/hw/9/unit_09/unit_09.js
+++ js_luschenko/hw/9/unit_09/unit_09.js
@@ -89,7 +89,11 @@ document.querySelector('.b-6').onclick = f6;
 let blocks7 = document.querySelectorAll('.out-7');
 
 function f7() {
-    //внутри цикла blocks7[i].classList....
+
+    for (let i = 0; i < blocks7.length; i++) {
+        blocks7[i].classList.add('bg-7');
+    }
+
 }
 
 document.querySelector('.b-7').onclick = f7;
