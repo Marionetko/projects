75e8c142d7757b7507b7c81987cea9d6d11f73b8
Chebotaiev Anton <webius.net@gmail.com>
2022-07-04 16:00:52 +0300

    t10 done

--- js_luschenko/hw/9/unit_09/unit_09.js
+++ js_luschenko/hw/9/unit_09/unit_09.js
@@ -133,13 +133,17 @@ for (let i = 0; i < div9.length; i++) {
 //  Task 10
 // Усложним предыдущие задачи. С помощью цикла повесьте на блоки out-10 событие клик. По клику должна выполняться функция f10. Функция, должна делать toggle класса bg-10 тому out-10 на котором кликнули.
 
-//let div10 = тут получите все out-10
+let div10 = document.querySelectorAll('.out-10');
 
 function f10() {
 
+    this.classList.toggle('bg-10')
+
 }
 
-// а тут цикл, похожий на предыдущее задание
+for (let i = 0; i < div10.length; i++) {
+    div10[i].onclick = f10;
+}
 
 
 
