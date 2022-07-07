3bcfbcce710f5aada87c279914c077888891c920
Chebotaiev Anton <webius.net@gmail.com>
2022-07-07 16:22:41 +0300

    t7 done

--- js_luschenko/hw/10/unit_10/unit_10.js
+++ js_luschenko/hw/10/unit_10/unit_10.js
@@ -115,7 +115,17 @@ let ar7 = ['china', 'india', 'brazil', 'japan', 'egypt'];
 
 function f7() {
 
-    ar7
+    ar7[7] = 'vietnam';
+    ar7[6] = 'turkey';
+    ar7[5] = 'italy';
+
+    let out = '';
+
+    for (let i = 0; i < ar7.length; i++) {
+        out += `${ar7[i]} `;
+    }
+
+    document.querySelector('.out-7').innerHTML = out;
 
 }
 
