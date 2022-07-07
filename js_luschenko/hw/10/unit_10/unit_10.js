56c5c6b043cd6aafebb92325f130c5eadee49851
Chebotaiev Anton <webius.net@gmail.com>
2022-07-07 15:33:29 +0300

    t4 done

--- js_luschenko/hw/10/unit_10/unit_10.js
+++ js_luschenko/hw/10/unit_10/unit_10.js
@@ -45,9 +45,10 @@ document.querySelector('.b-3').onclick = f3;
 // Вывод в out-4
 // Тест допустим массив [1,2,3,4,5,6,7,8,9,10] вывод: 1 4 9
 
-// let ar4 =  // переменную обьявляем эту здесь!!!!
+let ar4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // переменную обьявляем эту здесь!!!!
 
 function f4() {
+    document.querySelector('.out-4').innerHTML = `${ar4[0]} ${ar4[3]} ${ar4[8]}`;
 }
 
 document.querySelector('.b-4').onclick = f4;
